ffec962ba4782a078c17834ba49ac433b5a7da49
Henry Whitaker <henrywhitaker3@outlook.com>
2023-01-06 23:07:45 +0000

    Run the scheduler asynchronously and listen for interrupt

--- cmd/work.go
+++ cmd/work.go
@@ -5,6 +5,9 @@ Copyright © 2023 Henry Whitaker <henrywhitaker3@outlook.com>
 package cmd
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-co-op/gocron"
@@ -16,13 +19,13 @@ var workCmd = &cobra.Command{
 	Use:   "work",
 	Short: "Run the check daemon",
 	Long: `Run the program as a daemon.
-	
+
 	For each check where a cron value is specified, the daemon will run them when accordingly.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		s := gocron.NewScheduler(time.UTC)
 
 		for _, check := range cfg.Checks {
-			log.ForceInfof("Adding check '%s' to scheduler", check.Name)
+			log.Infof("Adding check '%s' to scheduler", check.Name)
 			s.Cron(check.Cron).Do(func() {
 				log.ForceInfof("Running schduled command '%s'", check.Name)
 				check.Execute()
@@ -30,7 +33,16 @@ var workCmd = &cobra.Command{
 		}
 
 		log.ForceInfo("Starting schduler")
-		s.StartBlocking()
+		s.StartAsync()
+
+		log.Info("Registering signal handlers for graceful shutdown")
+
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+		<-sig
+
+		log.ForceInfo("Stopping scheduler")
+		s.Stop()
 	},
 }
 
