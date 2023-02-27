110a56d67a0b9f762b54c16c8d74af303631293e
Henry Whitaker <henrywhitaker3@outlook.com>
2023-02-27 22:42:24 +0000

    added tests to retries func

--- internal/circuits/retries.go
+++ internal/circuits/retries.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"errors"
 	"time"
-
-	"github.com/henrywhitaker3/crog/internal/log"
 )
 
 type Effector func(context.Context) (any, error)
@@ -13,7 +11,6 @@ type Effector func(context.Context) (any, error)
 func Retry(e Effector, tries int, delay time.Duration) Effector {
 	return func(ctx context.Context) (any, error) {
 		for i := 0; i < tries; i++ {
-			log.Log.Debugf("Retry attempt %d", i+1)
 			out, err := e(ctx)
 			if err == nil || i >= (tries-1) {
 				return out, err
